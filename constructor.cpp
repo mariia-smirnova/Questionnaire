596186b5a6bb4d995374f4195f2f5c44ae112ba5
LAPTOP-4O74V1CA\Masha <smirnovamary.ru@gmail.com>
2023-10-27 22:02:46 +0300

    constructor test added

--- constructor.cpp
+++ constructor.cpp
@@ -1,11 +1,14 @@
 #include "constructor.h"
 #include "ui_constructor.h"
+#include <QPushButton>
 
 Constructor::Constructor(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Constructor)
 {
     ui->setupUi(this);
+    auto b = new QPushButton();
+    ui->left->addWidget(b);
 }
 
 Constructor::~Constructor()
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 
 #include <QApplication>
+#include "constructor.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    Constructor obj;
+    obj.show();
     return a.exec();
 }
